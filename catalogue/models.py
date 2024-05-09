7f07832ace5ff5a5dc2b3c812ea62dc6fdcefd90
TheVikramaditya <adhakare@yahoo.com>
2024-05-09 14:45:48 +0530

    Foreign Key , on_delete , self reference foreign key

--- catalogue/models.py
+++ catalogue/models.py
@@ -3,6 +3,26 @@ import uuid
 # Create your models here.
 
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=200,unique=True)
+    slug = models.SlugField(unique=True)
+    is_active = models.BooleanField()
+    
+    #self referencing foreign key , when a model needs to establish a relationship with itself
+    #i.e = Clothes > Shoes > Boot,  here Shoes parent will be Clothes , and Boot's will be Shoes
+    parent = models.ForeignKey('self',on_delete=models.PROTECT)
+    
+    
+class SeasonalEvent(models.Model):
+    id = models.BigAutoField(primary_key=True)#BigAutoField = auto increment field
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    name = models.CharField(max_length=200)
+
+
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200,null=False,blank=False)
     pid = models.CharField(max_length=200,null=False,blank=False)
@@ -22,17 +42,23 @@ class Product(models.Model):
         OUT_OF_STOCK: "Out of stock",
         BACKORDERED: "Back Ordered"
     }
-    status = models.CharField(max_length=3,choices=STOCK_STATUS,default=IN_STOCK)
+    stock_status = models.CharField(max_length=3,choices=STOCK_STATUS,default=IN_STOCK)
+    
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    seasonal_event = models.ForeignKey(SeasonalEvent,on_delete=models.SET_NULL,null=True)#some products are specially for some festivals
     
     def __str__(self):
         return self.name
     
+
+
 class ProductLine(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=2)
     sku = models.UUIDField(default=uuid.uuid4)
     stock_qty = models.IntegerField()
     order = models.IntegerField()
     weight = models.FloatField()
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
     
 
 class ProductImage(models.Model):
@@ -40,15 +66,12 @@ class ProductImage(models.Model):
     alternative_text = models.TextField()
     url = models.ImageField()
     order = models.IntegerField()
-    
-class Category(models.Model):
-    name = models.CharField(max_length=200,unique=True)
-    slug = models.SlugField(unique=True)
-    is_active = models.BooleanField()
-    
-class SeasonalEvent(models.Model):
-    id = models.BigAutoField(primary_key=True)#BigAutoField = auto increment field
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    name = models.CharField(max_length=200)
+    #ON_DELETE are of following types
+    # 1 - CASCADE = when a referenced row in the parent table is deleted, all corresponding rows in the child table will also be deleted automatically.
+    # 2 - PROTECT =  if there are any child rows referencing a parent row, the deletion of the parent row is prevented.
+    # 3 - SET_NULL = this option sets the foreign key in the child table to NULL
+    # 5 - SET_DEFAULT = this option sets the foreign key in the child table to a default value specified by the database schema.
+    # 6 - SET() = specify a custom value or expression to set the foreign key in the child table when a referenced row in the parent table is deleted.
+    #7  - DO_NOTHING = no action is taken on the corresponding rows in the child table.could potentially lead to referential integrity issues if not handled properly.
+    product_line = models.ForeignKey(ProductLine,on_delete=models.CASCADE)
     
