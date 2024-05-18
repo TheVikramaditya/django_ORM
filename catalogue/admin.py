42a327dd934d863373e7f4c5f2b0aae134fc426a
TheVikramaditya <adhakare@yahoo.com>
2024-05-18 14:19:00 +0530

    use of inlines

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -11,15 +11,29 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = ['id','name','is_active','parent']
     #  Allow editing of fields directly in the list
-    list_editable = ('name',)
+    # list_editable = ('name',)
     # list_display_links = ('id',)
 
 
 
 
+
+   
+
+
+
+class ProductLineInline(admin.StackedInline):
+    model = ProductLine
+    extra = 1#empty form to siplay
+    
+
 class ProductAdmin(admin.ModelAdmin):
     # add filters to the right side
     list_filter = ('stock_status',)
-    
+    inlines = [ProductLineInline]
+
+ 
+
 admin.site.register(Category,CategoryAdmin)#model, features
 admin.site.register(Product,ProductAdmin)
+# admin.site.register(P)
